5f29d907955a6fad3944813cd41326d301f1ec3e
Felix Nguyen <162833059+phucnguyen894@users.noreply.github.com>
2024-04-30 08:14:57 -0700

    Delete db directory

--- db/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.set("strictQuery", false);
-
-mongoose.connect('mongodb+srv://phucnguyen08051997:Phuc8947753@cluster0.eanwni9.mongodb.net/')
-    .then(() => console.log('Connected mongo db'))
-    .catch(e => console.log(e))
